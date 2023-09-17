fec349ca3bb8c06919e7a6a4e18b714e15f93af2
William Ratterman <williamratterman@MacBook-Pro.local>
2023-09-16 18:58:29 -0600

    refactor data operations to seperate module

---
+++ data_orchestrator/__init__.py
@@ -0,0 +1,2 @@
+from .data_loader import load_restaurant_data
+from .data_exporter import export_tables, export_cuisine_search_results
--- data_orchestrator/data_exporter.py
+++ data_orchestrator/data_exporter.py
@@ -1,53 +1,5 @@
 import csv
 
-from db import (
-    insert_restaurants,
-    insert_logo_photos,
-    insert_cuisines,
-    insert_menu,
-    insert_items,
-    insert_menu_items,
-    insert_restaurant_cuisines,
-)
-
-
-def load_restaurant_data(data, cur):
-    for restaurant_data in data["restaurants"]:
-        insert_restaurants(cur, restaurant_data)
-        restaurant_id = cur.fetchone()[0]
-        load_logo_photos(cur, restaurant_data, restaurant_id)
-        load_cuisines(cur, restaurant_data, restaurant_id)
-        load_menus(cur, restaurant_data, restaurant_id)
-        print(f"Successfully Loaded: {restaurant_data}")
-
-
-def load_menus(cur, restaurant_data, restaurant_id):
-    for menus_data in restaurant_data["menu"]:
-        insert_menu(cur, restaurant_id, menus_data)
-        menu_id = cur.fetchone()[0]
-        create_items(cur, menus_data, menu_id)
-
-
-def create_items(cur, menus_data, menu_id):
-    for items_data in menus_data["items"]:
-        insert_items(cur, items_data)
-        item_id = cur.fetchone()[0]
-        insert_menu_items(cur, menu_id, item_id)
-
-
-def load_cuisines(cur, restaurant_data, restaurant_id):
-    for cusine_data in restaurant_data["cuisines"]:
-        insert_cuisines(cur, cusine_data)
-        cur.execute("SELECT id FROM cuisines WHERE name = %s;", (cusine_data,))
-        cuisine_id = cur.fetchone()[0]
-        insert_restaurant_cuisines(cur, restaurant_id, cuisine_id)
-
-
-def load_logo_photos(cur, restaurant_data, restaurant_id):
-    for logo_images_data in restaurant_data["logo_photos"]:
-        insert_logo_photos(cur, restaurant_id, logo_images_data)
-
-
 def export_tables(cur):
     tables_to_export = [
         "restaurants",
